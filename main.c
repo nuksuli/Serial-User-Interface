c9efad246d7116a3693d78585743e473573bf3ff
Niko Kalliomaa <nikall@utu.fi>
2020-12-09 16:03:34 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: dtek0068
+ *
+ * Created on 09 December 2020, 16:02
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
